8426e7e21b690108408794884ed791bd1bc461d4
Johan Nordberg <its@johan-nordberg.com>
2022-09-18 09:41:53 +0200

    Update index.js

--- 01-creating-a-transaction/index.js
+++ 01-creating-a-transaction/index.js
@@ -1,21 +1,37 @@
-import { APIClient } from '@greymass/eosio'
-import { Contract } from 'NEWSDKNAME'
+import { SDK } from 'NEWSDKNAME'
 
-const client = new APIClient({ url: 'https://eos.greymass.com' })
+// SDK exposes all you need like anchor-link does, users don't need to import from core directly, if they need something they do
+// import { Asset } from 'NEWSDKNAME'
+
+const sdk = new SDK({ url: 'https://eos.greymass.com' }) // loaded with default plugins (something like if (browser) { plugins = [anchor-browser, atomichub] })
 
 async function main() {
+    const session = await sdk.login('myapp') // login also restores existing session unlike anchor-link, can have option for forcing re-login
+    
     // Loads contract by name
-    // Question: How could something like this do the API call?
-    //           Should we break this pattern for these kinds of calls?
-    const contract = client.getContract('rewards.gm')
-
+    const contract = await sdk.getContract('rewards.gm')
+    
+    // Work with table data
+    const usersTable = await contract.getTable('users')
+    const row = await usersTable.getByPrimary('teamgreymass')
+    const amount = row.getColumn('amount')
+    
+    // This could have a strongly typed interface as well
+    class MyRow extends Struct {
+        @Struct.field('uint64') declare amount: UInt64
+    }
+    const usersTable = await contract.getTable('users', MyRow)
+    const row = await usersTable.getByPrimary('teamgreymass')
+    const amount = row.amount
+    
     // Creates a claim action
     const action = contract.action('claim', {
         account: 'teamgreymass',
-    })
-
-    // Create a transaction
-    const transaction = Transaction.from({ actions: action })
+        amount,
+    }, {authority: [session.auth]}) // can be optional, set to to placeholder if omitted
+    
+    // Transact
+    await session.transact(action)
 }
 
 main()
