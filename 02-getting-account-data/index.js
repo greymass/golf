9b2db5a6200f6938fd566a0928d777d4417e8128
dafuga <danielfugere28@gmail.com>
2022-10-20 16:58:09 -0400

    chore: added account management methods
    issues #6, #8, #9 and #10

--- 02-getting-account-data/index.js
+++ 02-getting-account-data/index.js
@@ -13,6 +13,40 @@ async function main() {
 
     // Load account permissions
     const permissions = await account.getPermissions()
+
+    // Manage account Resources
+
+    // Buy ram
+    const buyRamEsr = await account.buyRam(1000)
+
+    // You could then pass it to Session Kit to sign and broadcast
+    // session.transact(buyRamEsr)
+
+    // Sell ram
+    const sellRamEsr = await account.sellRam(1000)
+
+    // Delegate bandwidth
+    const delegatebwEsr = await account.delegatebw(1000)
+
+    // Undelegate bandwidth
+    const undelegatebwEsr = await account.undelegatebw(1000)
+
+    // Delegate CPU
+    const delegateCPUEsr = await account.delegateCpu(1000)
+
+    // Undelegate CPU
+    const undelegateCpuesr = await account.undelegateCpu(1000)
+
+    // Manage account Permissions
+
+    // Add a new permission
+    const addPermissionEsr = await account.addPermission('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY', 'active')
+
+    // Remove a permission
+    const removePermissionEsr = await account.removePermission('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY')
+
+    // Update a permission
+    const updatePermissionEsr = await account.updatePermission('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqxxSWYY', 'active')
 }
 
 main()
