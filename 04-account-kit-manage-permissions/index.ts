499f63bd05d9587ab65d5db72f675b2ce7612d9e
dafuga <danielfugere28@gmail.com>
2022-10-25 17:57:39 -0400

    enhancement: renamed and added missing params to different methods

--- 04-account-kit-manage-permissions/index.ts
+++ 04-account-kit-manage-permissions/index.ts
@@ -1,15 +1,20 @@
 // SDK exposes all you need
-import { Chains, PublicKey, SigningRequest, Permission } from '@[SOMETHING]kit'
+import { Chains, PublicKey, Permission } from '@[SOMETHING]kit'
 import { Account } from '@[SOMETHING]kit/account'
+import { Session } from '@[SOMETHING]kit/session'
+
 
 async function main() {
-    // Loads account
-    const account: Account = await Account.load("teamgreymass", Chains.EOS)
+    const session = Session.login();
+    const account: Account = await session.getAccount();
+
+    // or
+    // const account: Account = await Account.load("teamgreymass", Chains.EOS)
 
     // Manage account Permissions
 
     // Get a permission
-    const permission: Permission = account.permissions.get("active")
+    const permission: Permission = account.getPermission("active")
 
     // Get its public keys
     const publicKeys: PublicKey[] = permission.keys
@@ -18,31 +23,43 @@ async function main() {
     const accounts: Account[] = permission.accounts
 
     // Add a new permission
-    const addPermissionEsr: SigningRequest = account.permissions.add('active', PublicKey.from('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY'))
+    await account.addPermission({
+        session, // optional, if not defined the active session which is globally stored will be used
+        permission: Permission.from({
+            permission: 'active',
+            parent: 'owner',
+            waits: [{ wait_sec: 10, weight: 1 }],
+            keys: [PublicKey.from('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY')]
+        }),
+    });
 
-    // Update a new permission
-    const updatePermissionEsr: SigningRequest = account.permissions.get('active').update(PublicKey.from('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY'))
+    // Update a permission
+    await permission.update(Permission.from({
+        session, // optional, if not defined the active session which is globally stored will be used
+        permission: 'owner',
+        keys: [PublicKey.from('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY')]
+    }));
 
     // Remove a permission
-    const removePermissionEsr: SigningRequest = account.permissions.get('active').remove()
+    await permission.remove()
 
     // Add a key to a permission
-    const addKeyEsr: SigningRequest = permission.keys.add(PublicKey.from('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY'))
+    await permission.addKey({ key: PublicKey.from('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY'), weight: 1 })
 
     // Remove a key from a permission
-    const removeKeyEsr: SigningRequest = permission.keys.remove(PublicKey.from('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY'))
+    await permission.removeKey(PublicKey.from('PUB_K1_5rcqJ8eNgNXooYyz9PTX2a2zZBktWaWezhXYGN2hrXWqfHSWYY'))
 
     // Add an account to a permission
-    const addAccountEsr: SigningRequest = permission.accounts.add(Account.from('teamgreymass'))
+    await permission.addAccount(Account.from('teamgreymass'))
 
     // Remove an account from a permission
-    const removeAccountEsr: SigningRequest = permission.accounts.remove(Account.from('teamgreymass'))
+    await permission.removeAccount(Account.from('teamgreymass'))
 
     // Add a wait to a permission
-    const addWaitEsr: SigningRequest = permission.wait.add(10)
+    await permission.addWait({ wait_sec: 10, weight: 1 })
 
     // Remove a wait from a permission
-    const removeWaitEsr: SigningRequest = permission.wait.remove()
+    await permission.removeWait({ wait_sec: 10, weight: 1 })
 }
 
 main()
